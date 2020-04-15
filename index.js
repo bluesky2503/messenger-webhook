5a06bc46331d7c7def86d93a33607239999f7198
bluesky2503 <63331836+bluesky2503@users.noreply.github.com>
2020-04-15 09:29:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -69,9 +69,7 @@ app.get('/webhook', (req, res) => {
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-  
- //res.send("Webhook trên Server Heroku sẳn sàng!");
-
+    
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
   
